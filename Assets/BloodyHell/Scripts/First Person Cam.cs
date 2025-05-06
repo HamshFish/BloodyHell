f07f1426818474faaea432ead2a128684e17e79a
ViperzzKing <viperzzkingg@gmail.com>
2025-05-06 16:22:57 +1000

    movement done now need to do cam

---
+++ Assets/BloodyHell/Scripts/First Person Cam.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    [SerializeField] float sens;
+
+    [SerializeField] Transform playerTransform;
+
+    [SerializeField] float minRotation = -30f;
+    [SerializeField] float maxRotation = 60f;
+
+    [SerializeField] float playerEyeOffset = 0.5f;
+
+    private float currentHorizontalRotation;
+    private float currentVerticalRotation;
+
+    private void Start()
+    {
+        currentHorizontalRotation = transform.localEulerAngles.y;
+        currentVerticalRotation = transform.localEulerAngles.x;
+    }
+
+
+}
--- Assets/BloodyHell/Scripts/Movement.cs
+++ Assets/BloodyHell/Scripts/Movement.cs
@@ -1,16 +1,90 @@
+using Mono.Cecil;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region State Setup
+
+    public enum State
     {
-        
+        Walk,
+        Rise,
+        Fall,
+        Run
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private State currentState;
+
+    private void Update()
     {
-        
+        switch (currentState)
+        {
+            case State.Walk:
+                break;
+            case State.Rise:
+                break;
+            case State.Fall:
+                break;
+            case State.Run:
+                break;
+        }
     }
+
+
+
+    #endregion
+
+    #region Temp Varibles
+
+    [SerializeField] private float speedWalk;
+    [SerializeField] private float jumpPower;
+
+    [SerializeField] private float gravityUp, gravityDown;
+
+    [SerializeField] LayerMask groundMask;
+
+    private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
+
+    [SerializeField] private int jumpsAllowed = 2;
+    private int jumpsRemaining;
+
+    #endregion
+
+    #region General
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+
+        jumpsRemaining = jumpsAllowed;
+    }
+
+
+
+    #endregion
+
+    #region Moving
+
+
+    private Vector3 GetMovementFromInput()
+    {
+
+        Vector2 inputThisFrame = new Vector2();
+
+        inputThisFrame.x = Input.GetAxis("Horizontal");
+        inputThisFrame.y = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(inputThisFrame.x, 0, inputThisFrame.y);
+
+        // moveDirection = transform.TransformDirection(moveDirection);
+    }
+
+    
+
+    #endregion
 }
