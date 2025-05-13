ac4062c8fc8d5ceeeb7166f0a1ac8093a9843e49
ViperzzKing <viperzzkingg@gmail.com>
2025-05-13 16:35:15 +1000

    starting on the attack

---
+++ Assets/BloodyHell/Scripts/Damage/ShootBlood.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ShootBlood : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private ParticleSystem blood;
+
+    [Header("Power")]
+    [SerializeField] private float shootSpeed;
+    [SerializeField] private float minRoll = 1;
+    [SerializeField] private float maxRoll = 20;
+
+
+
+    private void RollD20()
+    {
+
+    }
+
+    private void Projectile()
+    {
+
+    }
+}
